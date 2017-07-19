07ba636de02c0b2db6ae1284d3d7ccb8ab041736
Pablo.Ramirez <pablokrush@gmail.com>
2017-07-19 13:46:22 -0400

    Uploading profile pics

--- app/Http/Controllers/PerformerController.php
+++ app/Http/Controllers/PerformerController.php
@@ -75,10 +75,25 @@ class PerformerController extends Controller
     //Upload profile picture
     public function uploadProfilePicture()
     {
+        //no errors... good
+        $validFile = true;
+        if( $_FILES['profilePicture']['name'] ) {
+            //Don't want to use tmp name here
+            //$newFileNameWithPath = strtolower($_FILES['profilePicture']['tmp_name']);;
+            if( $_FILES['profilePicture']['size'] > 1024000 ){
+                $validFile = false;
+                $message = 'Oops! Your file\'s size is to large';
+            }
+            $baseDir = 'C:/wamp/www/camscript/uploads/'; //I'll make dynamic later, need to get out now
+            if($validFile) {
+                move_uploaded_file($_FILES['profilePicture']['tmp_name'], $baseDir . $_FILES['profilePicture']['name']);
+                $message = 'Congratulations! Your file was accepted';
+            }
+        } else {
+            $message = 'Oops! Your upload triggered the following error: ' . $_FILES['profilePicture']['error'];
+        }
 
-        $pablo = $_FILES;
-        $dora = $pablo;
-
+        session()->flash('message', $message);
         return view('performer.home');
 
     }
