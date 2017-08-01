058e41b9a9e73f306cdfdb52d6e962fe7e0dccf9
Pablo.Ramirez <pablokrush@gmail.com>
2017-08-01 14:00:53 -0400

    Set up Performer fillable

--- app/Performer.php
+++ app/Performer.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Performer extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'start_performing', 'end_performing', 'earning',
+    ];
 }
